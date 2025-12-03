55ea12dc4824de7fd6a25fcd21d92a509b7dc143
thamimkhonza-12 <thamimkhonza@gmail.com>
2025-12-03 20:15:31 +0200

    oop

---
+++ oop/polymorphism_demo,py.py
@@ -0,0 +1,23 @@
+import math
+
+class Shape:
+    def area(self):
+        """Base method to be overridden by derived classes."""
+        raise NotImplementedError("Subclasses must override this method.")
+
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius ** 2
